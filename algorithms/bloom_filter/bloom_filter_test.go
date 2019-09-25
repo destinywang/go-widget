a687cd3bf66ff76da4d5e4fb641996e9518220f5
destiny <destinywk@163.com>
2019-09-25 11:23:20 +0800

    [BloomFilter] tests
    Change-Id: Ia9cf7f143dbc4390fe161c0cf949e5066dac97e2

--- algorithms/bloom_filter/bloom_filter_test.go
+++ algorithms/bloom_filter/bloom_filter_test.go
@@ -1,6 +1,7 @@
 package bloom_filter
 
 import (
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -14,4 +15,23 @@ func TestBloomFilter(t *testing.T) {
 	assert.Nil(t, err)
 	contains = bf.Contains("destiny")
 	t.Logf("after: %t", contains)
+}
+
+func Test(t *testing.T) {
+	bf := &BloomFilter{}
+	bf.Init()
+	cache := make(map[string]struct{})
+
+	for i := 0; i < 10; i++ {
+		uuids, _ := uuid.NewV4()
+		uid := uuids.String()
+		inBloom := bf.Contains(uid)
+		_, inMap := cache[uid]
+		if inBloom != inMap {
+			t.Errorf("idx=[%d], uuid=[%s], inBloom=[%v], inMap=[%v]", i, uid, inBloom, inMap)
+		}
+
+		cache[uid] = struct{}{}
+		bf.Add(uid)
+	}
 }
